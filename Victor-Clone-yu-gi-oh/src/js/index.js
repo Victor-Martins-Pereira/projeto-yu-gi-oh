a27eb76ef8814856442637aad49d3b3187e0780e
Victor-Martins-Pereira <victorcriciuma@gmail.com>
2025-01-29 19:09:11 -0300

    Add files via upload

---
+++ Victor-Clone-yu-gi-oh/src/js/index.js
@@ -0,0 +1,45 @@
+const btnAvancar = document.getElementById("btn-avancar");
+const btnvoltar = document.getElementById("btn-voltar")
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+
+cartoes.forEach(cartao => {
+   cartao.addEventListener("click", function () {
+      const cartaVirada = cartao.querySelector(".carta-virada")
+
+      //Virar o cartão
+      cartao.classList.toggle("virar");
+      //Mostrar o fundo da carta
+      cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+      const descricao = cartao.querySelector(".descricao")
+      descricao.classList.toggle("esconder");
+   })
+})
+
+btnAvancar.addEventListener("click", function () {
+   if (cartaoAtual === cartoes.length - 1) return;
+
+   esconderCartaoSelecionado();
+
+   cartaoAtual++;
+   mostrarCartao(cartaoAtual);
+});
+
+btnvoltar.addEventListener("click", function () {
+
+   if (cartaoAtual === 0) return;
+
+   esconderCartaoSelecionado();
+
+   cartaoAtual--;
+   mostrarCartao(cartaoAtual);
+});
+function mostrarCartao() {
+   cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+   const cartaoSelecionado = document.querySelector(".selecionado");
+   cartaoSelecionado.classList.remove("selecionado");
+}
